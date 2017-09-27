bf72c9322464da6cbaaed2868ed059ef39e71c42
Hugo <hugo.le-dall@etu.univ-nantes.fr>
2017-09-27 08:47:37 +0200

    [Init] Commit initial

---
+++ src/main/java/webservice/backdata/spring/BackDatabaseApiApplication.java
@@ -0,0 +1,12 @@
+package webservice.backdata.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackDatabaseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackDatabaseApiApplication.class, args);
+	}
+}
