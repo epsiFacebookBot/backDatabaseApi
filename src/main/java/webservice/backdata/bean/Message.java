68c4156bfdf337655354ee0197f44e44ff9525db
alexandre.belhomme <alexandre.belhomme@capgemini.com>
2017-10-17 14:49:09 +0200

    Passage des attributs des beans en private

--- src/main/java/webservice/backdata/bean/Message.java
+++ src/main/java/webservice/backdata/bean/Message.java
@@ -4,56 +4,67 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class Message {
-	String mid;
-	String text;
-	String idFrom;
-	String idTo;
-	String attachmentType;
-	String payload;
-	
-	public Message(String mid,String text,String idFrom,String idTo){
-		this.mid = mid;
-		this.text = text;
-		this.idFrom = idFrom;
-		this.idTo = idTo;
-	}
-	public String getMid() {
-		return mid;
-	}
-	public void setMid(String mid) {
-		this.mid = mid;
-	}
-	public String getText() {
-		return text;
-	}
-	public void setText(String text) {
-		this.text = text;
-	}
-	public String getIdFrom() {
-		return idFrom;
-	}
-	public void setIdFrom(String idFrom) {
-		this.idFrom = idFrom;
-	}
-	public String getIdTo() {
-		return idTo;
-	}
-	public void setIdTo(String idTo) {
-		this.idTo = idTo;
-	}
-	
-	public String getPayload() {
-		return payload;
-	}
-	public void setPayload(String payload) {
-		this.payload = payload;
-	}
-	public String getAttachmentType() {
-		return attachmentType;
-	}
-	public void setAttachmentType(String attachmentType) {
-		this.attachmentType = attachmentType;
-	}
-	
+    private String mid;
+    private String text;
+    private String idFrom;
+    private String idTo;
+    private String attachmentType;
+    private String payload;
+
+    public Message(String mid, String text, String idFrom, String idTo) {
+        this.mid = mid;
+        this.text = text;
+        this.idFrom = idFrom;
+        this.idTo = idTo;
+    }
+
+    public String getMid() {
+        return mid;
+    }
+
+    public void setMid(String mid) {
+        this.mid = mid;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getIdFrom() {
+        return idFrom;
+    }
+
+    public void setIdFrom(String idFrom) {
+        this.idFrom = idFrom;
+    }
+
+    public String getIdTo() {
+        return idTo;
+    }
+
+    public void setIdTo(String idTo) {
+        this.idTo = idTo;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
+
+    public String getAttachmentType() {
+        return attachmentType;
+    }
+
+    public void setAttachmentType(String attachmentType) {
+        this.attachmentType = attachmentType;
+    }
+
 
 }
