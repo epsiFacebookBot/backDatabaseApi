3534c4e6e46e715cd9f51c63a04fde6df05bb69f
Hugo <hugo.le-dall@etu.univ-nantes.fr>
2017-10-20 18:34:45 +0200

    Fix TU

--- src/test/java/webservice/backdata/services/MessageServiceTest.java
+++ src/test/java/webservice/backdata/services/MessageServiceTest.java
@@ -7,6 +7,9 @@ import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,115 +26,126 @@ public class MessageServiceTest {
 	MessageService messageService;
 	@Mock
 	MessagePersistence messagePersistence;
-	
+	@Mock
+	private HttpClient client;
+
+	@PostConstruct
+	private void init() {
+
+		client = new HttpClient();
+		client.getParams().setParameter("http.useragent", "Test Client");
+
+	}
+
 	@Test
-	public void testGetMessageByMid_shouldReturnAMessage(){
+	public void testGetMessageByMid_shouldReturnAMessage() {
 		final String mid = "123";
 		Message message = createMessage();
 		when(messagePersistence.getMessageByMid("123")).thenReturn(message);
 		Message reponse = messageService.getMessageByMid(mid);
-		assertEquals(reponse.getMid(),message.getMid());
+		assertEquals(reponse.getMid(), message.getMid());
 		verify(messagePersistence).getMessageByMid(mid);
 	}
+
 	@Test
-	public void testSaveMessage_shouldSaveMessage_whenMessageOk() throws HttpException, IOException{
+	public void testSaveMessage_shouldSaveMessage_whenMessageOk() throws HttpException, IOException {
 		Message message = createMessage();
 		when(messagePersistence.saveMessage(message)).thenReturn(true);
 		boolean reponse = messageService.saveMessage(message);
-		assertEquals(reponse,true);
+		assertEquals(reponse, true);
 		verify(messagePersistence).saveMessage(message);
 	}
-	
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void testSaveMessage_shouldNotSaveMessage_whenMessageNull() throws HttpException, IOException{
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSaveMessage_shouldNotSaveMessage_whenMessageNull() throws HttpException, IOException {
 		Message message = null;
 		when(messagePersistence.saveMessage(message)).thenReturn(true);
 		boolean reponse = messageService.saveMessage(message);
-		assertEquals(reponse,true);
-		verify(messagePersistence,times(0)).saveMessage(message);
+		assertEquals(reponse, true);
+		verify(messagePersistence, times(0)).saveMessage(message);
 	}
-	
 
-	@Test(expected=IllegalArgumentException.class)
-	public void testSaveMessage_shouldNotSaveMessage_whenMidNull() throws HttpException, IOException{
+	@Test(expected = IllegalArgumentException.class)
+	public void testSaveMessage_shouldNotSaveMessage_whenMidNull() throws HttpException, IOException {
 		Message message = createMessage();
 		message.setMid(null);
 		when(messagePersistence.saveMessage(message)).thenReturn(true);
 		boolean reponse = messageService.saveMessage(message);
-		assertEquals(reponse,true);
-		verify(messagePersistence,times(0)).saveMessage(message);
+		assertEquals(reponse, true);
+		verify(messagePersistence, times(0)).saveMessage(message);
 	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void testSaveMessage_shouldNotSaveMessage_whenMidEmpty() throws HttpException, IOException{
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSaveMessage_shouldNotSaveMessage_whenMidEmpty() throws HttpException, IOException {
 		Message message = createMessage();
 		message.setMid("");
 		when(messagePersistence.saveMessage(message)).thenReturn(true);
 		boolean reponse = messageService.saveMessage(message);
-		assertEquals(reponse,true);
-		verify(messagePersistence,times(0)).saveMessage(message);
+		assertEquals(reponse, true);
+		verify(messagePersistence, times(0)).saveMessage(message);
 	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void testSaveMessage_shouldNotSaveMessage_whenTextNull() throws HttpException, IOException{
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSaveMessage_shouldNotSaveMessage_whenTextNull() throws HttpException, IOException {
 		Message message = createMessage();
 		message.setText(null);
 		when(messagePersistence.saveMessage(message)).thenReturn(true);
 		boolean reponse = messageService.saveMessage(message);
-		assertEquals(reponse,true);
-		verify(messagePersistence,times(0)).saveMessage(message);
+		assertEquals(reponse, true);
+		verify(messagePersistence, times(0)).saveMessage(message);
 	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void testSaveMessage_shouldNotSaveMessage_whenTextEmpty() throws HttpException, IOException{
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSaveMessage_shouldNotSaveMessage_whenTextEmpty() throws HttpException, IOException {
 		Message message = createMessage();
 		message.setText("");
 		when(messagePersistence.saveMessage(message)).thenReturn(true);
 		boolean reponse = messageService.saveMessage(message);
-		assertEquals(reponse,true);
-		verify(messagePersistence,times(0)).saveMessage(message);
+		assertEquals(reponse, true);
+		verify(messagePersistence, times(0)).saveMessage(message);
 	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void testSaveMessage_shouldNotSaveMessage_whenIdFromNull() throws HttpException, IOException{
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSaveMessage_shouldNotSaveMessage_whenIdFromNull() throws HttpException, IOException {
 		Message message = createMessage();
 		message.setIdFrom(null);
 		when(messagePersistence.saveMessage(message)).thenReturn(true);
 		boolean reponse = messageService.saveMessage(message);
-		assertEquals(reponse,true);
-		verify(messagePersistence,times(0)).saveMessage(message);
+		assertEquals(reponse, true);
+		verify(messagePersistence, times(0)).saveMessage(message);
 	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void testSaveMessage_shouldNotSaveMessage_whenIdFromEmpty() throws HttpException, IOException{
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSaveMessage_shouldNotSaveMessage_whenIdFromEmpty() throws HttpException, IOException {
 		Message message = createMessage();
 		message.setIdFrom("");
 		when(messagePersistence.saveMessage(message)).thenReturn(true);
 		boolean reponse = messageService.saveMessage(message);
-		assertEquals(reponse,true);
-		verify(messagePersistence,times(0)).saveMessage(message);
+		assertEquals(reponse, true);
+		verify(messagePersistence, times(0)).saveMessage(message);
 	}
-	@Test(expected=IllegalArgumentException.class)
-	public void testSaveMessage_shouldNotSaveMessage_whenIdToNull() throws HttpException, IOException{
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSaveMessage_shouldNotSaveMessage_whenIdToNull() throws HttpException, IOException {
 		Message message = createMessage();
 		message.setIdTo(null);
 		when(messagePersistence.saveMessage(message)).thenReturn(true);
 		boolean reponse = messageService.saveMessage(message);
-		assertEquals(reponse,true);
-		verify(messagePersistence,times(0)).saveMessage(message);
+		assertEquals(reponse, true);
+		verify(messagePersistence, times(0)).saveMessage(message);
 	}
-	@Test(expected=IllegalArgumentException.class)
-	public void testSaveMessage_shouldNotSaveMessage_whenIdToEmpty() throws HttpException, IOException{
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSaveMessage_shouldNotSaveMessage_whenIdToEmpty() throws HttpException, IOException {
 		Message message = createMessage();
 		message.setIdTo("");
 		when(messagePersistence.saveMessage(message)).thenReturn(true);
 		boolean reponse = messageService.saveMessage(message);
-		assertEquals(reponse,true);
-		verify(messagePersistence,times(0)).saveMessage(message);
+		assertEquals(reponse, true);
+		verify(messagePersistence, times(0)).saveMessage(message);
 	}
-	
-	private Message createMessage(){
+
+	private Message createMessage() {
 		Message message = new Message("123", "Salut!", "942", "1357");
 		return message;
 	}
