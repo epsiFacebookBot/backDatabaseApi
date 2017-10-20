936cf762cd234f515abc9d445761cdbb13642c35
Hugo <hugo.le-dall@etu.univ-nantes.fr>
2017-10-20 18:06:08 +0200

    Fix saveUser

--- src/main/java/webservice/backdata/controllers/UserController.java
+++ src/main/java/webservice/backdata/controllers/UserController.java
@@ -3,11 +3,14 @@ package webservice.backdata.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 import webservice.backdata.bean.User;
 import webservice.backdata.services.UserService;
@@ -27,9 +30,11 @@ public class UserController {
         return userService.getUsers();
     }
 
-    @RequestMapping(value = "/user/add", method = RequestMethod.POST)
-    public boolean saveUser(@RequestParam(value = "id", required = true) String id, @RequestParam(value = "name", required = true) String name) {
-        return userService.saveUser(new User(id, name));
+    @RequestMapping(value = "/user/add", method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public boolean saveUser(@RequestBody JsonNode userJson) {
+        return userService.saveUser(UserService.getUserFromJson(userJson));
     }
 
 }
